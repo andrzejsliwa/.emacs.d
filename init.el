503ad22a47363f442ba0d295ed9a38ba793be8df
Andrzej Sliwa <andrzej.sliwa@i-tool.eu>
2014-08-25 12:06:10 +0200

    fix for web templates

--- init.el
+++ init.el
@@ -706,6 +706,23 @@ Repeated invocations toggle between the two most recently open buffers."
 (require 'pbcopy)
 (turn-on-pbcopy)
 
+(personal-require-packages '(web-mode))
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[gj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.scss\\'" . web-mode))
+(setq web-mode-markup-indent-offset 2)
+(setq web-mode-css-indent-offset 2)
+(setq web-mode-code-indent-offset 2)
+
+
 ;; +--
 ;; | RUBY
 ;; +-------------------+
